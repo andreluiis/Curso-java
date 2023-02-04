f4a218c5d9f30152af6a889c2577fefce22efc09
andre <andreluis121003@gmail.com>
2023-02-03 21:06:18 -0300

    commit0

---
+++ proj/proj-java/proj-java/src/main/java/org/senai/projjava/ProjJavaApplication.java
@@ -0,0 +1,13 @@
+package org.senai.projjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjJavaApplication.class, args);
+	}
+
+}
---
+++ proj/proj-java/proj-java/src/test/java/org/senai/projjava/ProjJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senai.projjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
